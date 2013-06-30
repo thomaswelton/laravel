faff226029f1c53ff08f23a576e82cc7e0b0bcf3
Thomas Welton <thomaswelton@me.com>
2013-06-30 19:13:07 +0100

    Added users migration

---
+++ app/database/migrations/2013_06_30_171413_create_users.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($table) {
+			$table->increments('id');
+
+			$table->string('username', 32);
+			$table->string('email', 320);
+			$table->string('password', 64);
+			
+			$table->integer('role');
+			
+			$table->boolean('active');
+
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
