6cda65ff3c636d44baedcd6b94a3d0be3b321e39
Thomas Welton <thomaswelton@me.com>
2013-07-14 22:58:26 +0100

    Installed facebook component

--- app/config/app.php
+++ app/config/app.php
@@ -113,6 +113,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'ThomasWelton\Facebook\FacebookServiceProvider'
 
 	),
 
@@ -178,6 +179,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Facebook'		  => 'ThomasWelton\Facebook\Facades\Facebook'
 
 	),
 
