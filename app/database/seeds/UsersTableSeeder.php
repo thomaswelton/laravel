28309677ec4ef68c27150e4211c25fc8a2931cd6
Thomas Welton <thomas@clicklabs.co.uk>
2013-08-01 11:43:58 +0100

    updated user table seeder

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -4,19 +4,10 @@ class UsersTableSeeder extends Seeder {
 
     public function run(){
 
-		Sentry::register(array(
-                'email' => 'user@example.com',
-                'password' => 'password'
-        ));
-
         Sentry::register(array(
                 'email' => 'admin@example.com',
-                'password' => 'password'
-        ));
-
-		Sentry::register(array(
-                'email' => 'superadmin@example.com',
-                'password' => 'password'
+                'password' => 'password',
+                'activated' => 1
         ));
 	}
 }
