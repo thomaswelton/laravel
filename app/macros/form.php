cd88d414b11b580222fbea4c018ed905332cc979
Thomas Welton <thomas@clicklabs.co.uk>
2013-08-19 14:51:16 +0100

    Added date form macro, and update control macro with $options as used by Laravel core

--- app/macros/form.php
+++ app/macros/form.php
@@ -1,14 +1,30 @@
 <?php
 
-Form::macro('control', function($type, $field, $title = null, $value = '')
+Form::macro('control', function($type, $name, $title = null, $value = null, $options = array())
 {
 	if(is_null($title)) $title = ucfirst($field);
-	
+
 	$data = array(
-		'label' => Form::label($field, $title, array('class' => 'control-label')),
-		'input' => Form::$type($field, $value),
-		'field' => $field
+		'label' => Form::label($name, $title, array('class' => 'control-label')),
+		'field' => $name
 	);
 
+	if(is_null($value)){
+		$data['input'] = Form::$type($name);
+	}else{
+		$data['input'] = Form::$type($name, $value, $options);
+	}
+
 	return View::make('macros/form_control', $data);
+});
+
+Form::macro('date', function($name, $value = null, $options = array())
+{
+	$value = Form::getValueAttribute($name, $value);
+
+	if('Carbon\Carbon' == get_class($value)){
+		$value = $value->toDateString();
+	}
+
+	return Form::input('date', $name, $value, $options);
 });
