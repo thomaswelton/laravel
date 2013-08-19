c1e6d64db6bad19115cace434a0d8c43638f9c17
Thomas Welton <thomas@clicklabs.co.uk>
2013-08-19 14:20:45 +0100

    Added missing closing quote for alert type warning

--- app/macros/html.php
+++ app/macros/html.php
@@ -7,7 +7,7 @@ HTML::macro('flash', function($syntax = null) {
     foreach ($alert_types as $type) {
         if( Session::has($type) ) {
             $output = '<div class="alert';
-            $output .= ($type == 'warning') ? '>' : ' alert-'. $type .'">';
+            $output .= ($type == 'warning') ? '">' : ' alert-'. $type .'">';
             $output .= '<button type="button" class="close" data-dismiss="alert">&times;</button>';
             $output .=  Session::get($type) . '</div>';
         }
