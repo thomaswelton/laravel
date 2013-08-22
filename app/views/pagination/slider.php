baaef307296a1c59d8bab90b514197ce8ab662d5
Thomas Welton <thomas@clicklabs.co.uk>
2013-08-22 12:12:49 +0100

    Updated pagination view for twbs 3.0

---
+++ app/views/pagination/slider.php
@@ -0,0 +1,11 @@
+<?php
+	$presenter = new Illuminate\Pagination\BootstrapPresenter($paginator);
+?>
+
+<?php if ($paginator->getLastPage() > 1): ?>
+	<div class="col-lg-12 text-center">
+		<ul class="pagination">
+			<?php echo $presenter->render(); ?>
+		</ul>
+	</div>
+<?php endif; ?>
