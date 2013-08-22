188e6f46aafe6bd7d0bebead99f9fa3035aa3155
Thomas Welton <thomas@clicklabs.co.uk>
2013-08-22 12:11:57 +0100

    Added more admin users

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -4,10 +4,18 @@ class UsersTableSeeder extends Seeder {
 
     public function run(){
 
-        Sentry::register(array(
-                'email' => 'admin@example.com',
-                'password' => 'password',
-                'activated' => 1
-        ));
+		Sentry::register(array(
+	            'email' => 'admin@example.com',
+	            'password' => 'password',
+	            'activated' => 1
+	    ));
+
+        for ($i=1; $i <= 5; $i++) {
+        	Sentry::register(array(
+	                'email' => 'admin' . $i . '@example.com',
+	                'password' => 'password',
+	                'activated' => 1
+	        ));
+        }
 	}
 }
