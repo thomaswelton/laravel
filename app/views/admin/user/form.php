66abf5a69593006da206eb514e20b0f5ba081a5a
Thomas Welton <thomaswelton@me.com>
2013-09-01 15:01:39 +0100

    Added help text and created/update fields to the user form

--- app/views/admin/user/form.php
+++ app/views/admin/user/form.php
@@ -11,12 +11,20 @@ if(isset($user)): ?>
 <?php endif; ?>
 
     <?= Form::control('email', 'email', 'Email') ?>
-    <?= Form::control('password', 'password') ?>
+
+    <? if(isset($user)): ?>
+        <?= Form::control('password', 'password')->with('help', 'Leave empty to remain unchanged') ?>
+        <?= Form::control('static', 'created_at', 'Date created') ?>
+        <?= Form::control('static', 'updated_at', 'Last Edit') ?>
+    <? else: ?>
+        <?= Form::control('password', 'password') ?>
+    <? endif ?>
 
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-6">
+            <hr>
             <button type="submit" name="action" value="save" class="btn btn-primary">Save changes</button>
-            <a href="<?= action('Admin\\UserController@index') ?>" class="btn">Cancel</a>
+            <a href="<?= action('Admin\\UserController@index') ?>" class="btn btn-link">Cancel</a>
         </div>
     </div>
 
