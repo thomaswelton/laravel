53ca4a664b330b45feaaf9994031ee7b0d827f92
Thomas Welton <thomas.welton@helloworldlondon.co.uk>
2013-09-19 15:49:18 +0100

    added link account prompt to admin homepage

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -1,6 +1,15 @@
 @extends('admin.layouts.default')
 
 @section('content')
-	<?= HTML::page_header('Administration') ?>
-	<?= HTML::flash() ?>
+	{{ HTML::page_header('Administration') }}
+	{{ HTML::flash() }}
+
+	@if (!is_object(Auth::user()->facebook))
+		<h2>Link your accounts</h2>
+		<p class="lead">Want a simpler faster login?</p>
+		<p>Link your Facebook profile to your account and get a one click login to the admin tool</p>
+		<a href="<?= OAuth::associate('facebook')->redirect(Request::url()) ?>" class="btn btn-primary">Link accounts</a>
+	@endif
 @stop
+
+
