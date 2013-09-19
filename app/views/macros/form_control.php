b348184102b93555ea3fcb9f2a306882a79342f5
Thomas Welton <thomaswelton@me.com>
2013-09-19 21:46:08 +0100

    update form control macro to use bootstrpa 3

--- app/views/macros/form_control.php
+++ app/views/macros/form_control.php
@@ -1,9 +1,9 @@
-<div class="form-group <?php if($errors->has($field)) echo 'error' ?>">
+<div class="form-group <?php if($errors->has($field)) echo 'has-error' ?>">
     <?= $label ?>
     <div class="col-sm-9">
         <?= $input ?>
         <?php if($errors->has($field)): ?>
-            <span class="help-inline"><?= $errors->first($field) ?></span>
+            <span class="help-block"><?= $errors->first($field) ?></span>
         <?php endif; ?>
         <? if(isset($help)): ?>
         	<span class="help-block"><?= $help ?></span>
