7ae166702148d5256332853cfd88038940150468
David Thingsaker <david.thingsaker@helloworldlondon.co.uk>
2013-10-24 12:38:55 +0100

    added refactored page headers with buttons feature

--- app/views/admin/user/index.blade.php
+++ app/views/admin/user/index.blade.php
@@ -1,20 +1,7 @@
 @extends('admin.layouts.default')
 
 @section('content')
-    <div class="page-header row">
-        <div class="col-sm-7">
-            <h1>Users</h1>
-        </div>
-
-        <div class="col-sm-5 pull-left">
-            <span class="pull-right">
-                <div class="btn-group">
-                    <a class="btn btn-info" href="<?= Request::fullUrl() ?>?&amp;format=csv"><?= HTML::icon('download-alt') ?> Download CSV</a>
-                    <a class="btn btn-success" href="/admin/users/create"><?= HTML::icon('plus-sign') ?> New Entry</a>
-                </div>
-            </span>
-        </div>
-    </div>
+    {{ HTML::page_header('Users')->with('button', array('new', 'csv'))}}
 
     <?= HTML::flash() ?>
 
