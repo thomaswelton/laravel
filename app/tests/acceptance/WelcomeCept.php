f0b4b0cf13cc7cafa9ec79fc5b766c0fd5912188
thomaswelton <thomaswelton@me.com>
2014-02-23 21:17:42 +0000

    add codeception test

--- app/tests/acceptance/WelcomeCept.php
+++ app/tests/acceptance/WelcomeCept.php
@@ -1,5 +1,22 @@
 <?php
+
 $I = new WebGuy($scenario);
 $I->wantTo('ensure that frontpage works');
 $I->amOnPage('/');
 $I->see('You have arrived.','h1');
+
+$I = new WebGuy($scenario);
+$I->wantTo('ensure that the admin form requires password');
+$I->amOnPage('/admin/login');
+$I->fillField('#email', 'davert');
+$I->click('form button[type=submit]');
+$I->see('Password field is required.');
+
+$I = new WebGuy($scenario);
+$I->wantTo('ensure that the admin form works');
+$I->amOnPage('/admin/login');
+$I->fillField('#email', 'admin@example.com');
+$I->fillField('#password', 'password');
+$I->click('form button[type=submit]');
+$I->seeCurrentUrlEquals('/admin');
+$I->see('Administration', 'h1');
